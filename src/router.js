ae5f477e85b3e79f66a3ff6be7fd022d03ccd375
rofiq999 <rofiqhidayat495@gmail.com>
2022-11-16 23:18:57 +0700

    add router/notification

--- src/router.js
+++ src/router.js
@@ -1,7 +1,11 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from 'react-router-dom';
 
-import Login from "./pages/Login";
+import Login from './pages/Login';
+import Notification from './pages/Notification';
 
-const router = createBrowserRouter([{ path: "/login", element: <Login /> }]);
+const router = createBrowserRouter([
+  { path: '/login', element: <Login /> },
+  { path: '/notification', element: <Notification /> },
+]);
 
 export default router;
