c22ece7c1f9870743449b361444900108b8e3cac
rsat100 <rsatbudiman@gmail.com>
2022-11-16 20:09:51 +0700

    16/11/2022

--- src/router.js
+++ src/router.js
@@ -2,10 +2,12 @@ import { createBrowserRouter } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Forgot from "./pages/Forgot";
 
 const router = createBrowserRouter([
   { path: "/login", element: <Login /> },
   { path: "/register", element: <Register /> },
+  { path: "/forgot", element: <Forgot /> },
 ]);
 
 export default router;
