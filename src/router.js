1da9e18161edf58f9fdc6fdddc2de1993bbed2df
MUH. AHSAN <muh.ahsan.student@gmail.com>
2022-11-17 00:28:09 +0700

    Updated router

--- src/router.js
+++ src/router.js
@@ -1,10 +1,11 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Forgot from "./pages/Forgot";
 import Profile from "./pages/Profile";
+import Notification from "./pages/Notification";
 
 const router = createBrowserRouter([
   { path: "/", element: <Home /> },
@@ -12,6 +13,7 @@ const router = createBrowserRouter([
   { path: "/register", element: <Register /> },
   { path: "/forgot", element: <Forgot /> },
   { path: "/profile", element: <Profile /> },
+  { path: "/notification", element: <Notification /> },
 ]);
 
 export default router;
