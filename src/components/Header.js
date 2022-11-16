d355b713e2612f7390a3860e216da2720f88408d
rsat100 <rsatbudiman@gmail.com>
2022-11-16 16:26:13 +0700

    16/11/2022

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,41 @@
 import React, { Component, Fragment } from "react";
+import styles from "../styles/Login.module.css";
+
+import mag from "../assets/mag.png";
+import love from "../assets/love.png";
+import cart from "../assets/cart.png";
+import chev from "../assets/chevron.png";
+import bar1 from "../assets/menu-1.png";
+import bar2 from "../assets/menu-2.png";
 
 class Header extends Component {
   render() {
     return (
       <Fragment>
-        <main>
-          <h1>RAZ</h1>
+        <main className={styles["main"]}>
+          <h1 className={styles["header-left"]}>RAZ</h1>
+          <div className={styles["header-center"]}>
+            <p className={styles["header-center-text"]}>HOME</p>
+            <div className={styles["header-center-div"]}>
+              <p className={styles["header-center-text"]}>PAGES</p>
+              <img className={styles["chevron"]} src={chev} alt="img" />
+            </div>
+            <div className={styles["header-center-div"]}>
+              <p className={styles["header-center-text"]}>SHOP</p>
+              <img className={styles["chevron"]} src={chev} alt="img" />
+            </div>
+            <p className={styles["header-center-text"]}>BLOG</p>
+          </div>
+          <div className={styles["header-right"]}>
+            <img className={styles["image-1"]} src={mag} alt="img" />
+            <img className={styles["image-2"]} src={love} alt="img" />
+            <img className={styles["image-3"]} src={cart} alt="img" />
+            <div className={styles["header-right-menu"]}>
+              <img className={styles["menu-1"]} src={bar1} alt="img" />
+              <img className={styles["menu-2"]} src={bar2} alt="img" />
+              <img className={styles["menu-3"]} src={bar1} alt="img" />
+            </div>
+          </div>
         </main>
       </Fragment>
     );
