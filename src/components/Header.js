8cd134f8290c8cbd748759154faea8d9df9a76b8
MUH. AHSAN <muh.ahsan.student@gmail.com>
2022-11-20 12:33:14 +0700

    Updated Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from "react";
 import styles from "../styles/Header.module.css";
 
+import { Link } from "react-router-dom";
+
 import mag from "../assets/mag.png";
 import love from "../assets/love.png";
 import cart from "../assets/cart.png";
@@ -15,14 +17,24 @@ class Header extends Component {
         <main className={`${styles["main"]} ${styles["display-main"]}`}>
           <h1 className={styles["header-left"]}>RAZ</h1>
           <div className={styles["header-center"]}>
-            <p className={styles["header-center-text"]}>HOME</p>
+            <Link to={`/`} className={styles["header-center__link"]}>
+              <p className={styles["header-center-text"]}>HOME</p>
+            </Link>
             <div className={styles["header-center-div"]}>
               <p className={styles["header-center-text"]}>PAGES</p>
               <img className={styles["chevron"]} src={chev} alt="img" />
             </div>
-            <div className={styles["header-center-div"]}>
+            <div
+              className={`${styles["header-center-div"]} ${styles["header-center-div-shop"]}`}
+            >
               <p className={styles["header-center-text"]}>SHOP</p>
               <img className={styles["chevron"]} src={chev} alt="img" />
+              <span className={styles["header-center-div__contents"]}>
+                <Link to={`/products`} className={styles["link"]}>
+                  <p>Orders</p>
+                </Link>
+                <p>My Product</p>
+              </span>
             </div>
             <p className={styles["header-center-text"]}>BLOG</p>
           </div>
@@ -36,6 +48,14 @@ class Header extends Component {
               <img className={styles["menu-1"]} src={bar1} alt="img" />
               <img className={styles["menu-2"]} src={bar2} alt="img" />
               <img className={styles["menu-3"]} src={bar1} alt="img" />
+              <span className={`${styles["header-right-toggle__contents"]}`}>
+                <Link to={`/login`} className={styles["link"]}>
+                  <p>Login</p>
+                </Link>
+                {/* <Link to={`/profile`} className={styles["link"]}>
+                  <p>Profile</p>
+                </Link> */}
+              </span>
             </div>
           </div>
         </main>
