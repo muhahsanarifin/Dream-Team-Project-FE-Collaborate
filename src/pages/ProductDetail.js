1ee5a92fd4e07ab2dec3de4f4fc76ca30865f8e7
Diankrs30 <diankrs30@gmail.com>
2022-11-22 00:11:43 +0700

    add cart empty

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
