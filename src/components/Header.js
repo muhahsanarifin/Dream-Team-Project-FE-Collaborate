5918b18ac2b20ba659abbd3ffb94fedf1b342b88
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-01-06 23:02:31 +0700

    Updated Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -54,7 +54,7 @@ export default function Header({
         timerProgressBar: true,
       }).then((result) => {
         if (result.dismiss === Swal.DismissReason.timer) {
-          this.props.navigate("/");
+          navigate("/");
         }
       });
     };
